cfabd82200487608506aa4de1825abbe626f8f36
timtoronto634 <tim225x@gmail.com>
2023-09-08 01:00:08 +0900

    fix: use echo wraphandler

--- presentation/handler.go
+++ presentation/handler.go
@@ -1,9 +1,13 @@
 package presentation
 
 import (
+	"context"
+	slotv1 "echo-me/presentation/gen/slot/v1"
+	"echo-me/presentation/gen/slot/v1/slotv1connect"
 	"errors"
 	"net/http"
 
+	"connectrpc.com/connect"
 	sentryecho "github.com/getsentry/sentry-go/echo"
 	"github.com/labstack/echo/v4"
 )
@@ -12,11 +16,24 @@ type Server struct {
 	e *echo.Echo
 }
 
+type slotService struct{}
+
+func (s slotService) SaveSlot(ctx context.Context, req *connect.Request[slotv1.SaveSlotRequest]) (*connect.Response[slotv1.SaveSlotResponse], error) {
+	res := connect.NewResponse(&slotv1.SaveSlotResponse{
+		Id: 1,
+	})
+	return res, nil
+}
+
 func SetupServer() *Server {
 	s := &Server{
 		e: echo.New(),
 	}
 	s.e.Use(sentryecho.New(sentryecho.Options{Repanic: false}))
+	slotService := slotService{}
+	path, handler := slotv1connect.NewSaveSlotServiceHandler(slotService)
+
+	s.e.POST(path+"SaveSlot", echo.WrapHandler(handler))
 	s.e.PUT("/slots/:id", saveSlot)
 	s.e.GET("/slots", getSlots)
 	s.e.GET("/er", getError)
